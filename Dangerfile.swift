91db6ae66f79b498dd96701a4e386d1aa7ecca01
eduardojordan <eduardojordan@hotmail.com>
2021-07-29 19:20:13 +0200

    Include files danger

---
+++ Dangerfile.swift
@@ -0,0 +1,14 @@
+import Danger 
+let danger = Danger()
+
+// TEST MESSAGE FROM DANGER ***************************
+danger.message("Hello from Danger GITHUB Prueba DangerFile")
+
+// CAMBIOS DE VERSION  ***************************
+if danger.git.modifiedFiles.contains(where: { $0.contains(".xcworkspacedata")}) {
+        warn(" WARNING !!! Se han realizado cambios a nivel de .xcproject")
+}
+// CAMBIOS DE LIBRERIA  ***************************
+if danger.git.modifiedFiles.contains(where: { $0.contains("Pods")}) {
+        warn(" WARNING !!! CAMBIO EN LIBRERIAS DE APP ")
+    }
