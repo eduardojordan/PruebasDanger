48524b59047b09bb233436a7fbbf22c66e19d6f9
eduardojordan <eduardojordan@hotmail.com>
2021-07-30 18:21:38 +0200

    Gññre

--- PruebasDanger/ViewController.swift
+++ PruebasDanger/ViewController.swift
@@ -3,7 +3,7 @@
 //  PruebasDanger
 //
 //  Created by Eduardo Jordán Muñoz on 29/7/21.
-//xxxxgdgsssddsssddedffrrdddfdfdcxrdds3ddidcsesdsnndsesddddd
+//xxxxgdrgsssddsssddedffrrdddfdfdcxrdds3ddidcsesdsnndsesddddd
 
 
 import UIKit
