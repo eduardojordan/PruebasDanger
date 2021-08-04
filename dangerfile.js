dc945c2470ec009f9d0c16c239ba5f5f1e377ba2
eduardojordan <eduardojordan@hotmail.com>
2021-08-04 11:09:15 +0200

    prueba1eeeee3

--- dangerfile.js
+++ dangerfile.js
@@ -1,46 +1,130 @@
-const {danger, warn} = require('danger')
+
+import { danger } from "danger"
+
 const docsSwift = danger.git.fileMatch("**/*.swift")
 
-//const { exec } = require('child_process');
- const { exec } = require('child_process');
+// const { exec } = require('child_process');
+// const shell = require('shelljs')
+
+// var valorRetorno = '0'
+
+if (docsSwift.edited) {
+ fail("Se han realizado cambios en el proyecto archivo .swift")
+
+//enviar respuesta a otro script que sera llamado en pre-commit
+// return valorRetorno = '3'
+// console.log( valorRetorno)
+ } 
 
 
-// if (docsSwift.edited) {
-//  fail("Se han realizado cambios en el proyecto archivo .swift")
-// // return 1
-// } 
-// console.log(callTimes()); 
- // process.exit(1);
- // exec(child_process.spawnSync('echo', ['abc']));
-// process.on('exit', function() { process.exit(1); });
-// return console.log(callTimes());
-
-// process.exit(1);
-//
-//  const { exec } = require('child_process');
-//  exec("var= 1",(error, stdout, stderr) => {console.log(stdout);console.log(stderr);});
-
-// // return  exec("so [-tag]")
- // exec("echo $?")
+// console.log(valorRetorno)
+// ACA DEBERIA EXPORTAR ESTE VALOR PARA PRE_COMMIT
 
 
 
-// function callTimes() {
-//   let count = 1;
-//   return process.exit(1);
+
+
+
+
+//process.exit(1);
+// console.log($?)
+
+//exec("tag",(error, stdout, stderr) => {console.log(stdout);console.log(stderr);});
+ // result = 1
+// console.log(1)
+// process.stdout.write(1);
+
+
+  // exec("echo ANTES DE $valor",
+  //       (error, stdout, stderr) => {
+  //           console.log(stdout);
+  //           console.log(stderr);
+  //           if (error !== null) {
+  //               console.log(`exec error: ${error}`);
+  //           }
+  //       });
+
+  // exec("declare -r valor = 2"),
+  //       (error, stdout, stderr) => {
+  //           console.log(stdout);
+  //           console.log(stderr);
+  //           if (error !== null) {
+  //               console.log(`exec error: ${error}`);
+  //           }
+  //       });
+
+  // exec("echo DESPUES DE valor ${!valor}",
+  //       (error, stdout, stderr) => {
+  //           console.log(stdout);
+  //           console.log(stderr);
+  //           if (error !== null) {
+  //               console.log(`exec error: ${error}`);
+  //           }
+  //       });
+
+
+ 
+
+  // exec('echo ANTES DE $?',
+  //       (error, stdout, stderr) => {
+  //           console.log(stdout);
+  //           console.log(stderr);
+  //           if (error !== null) {
+  //               console.log(`exec error: ${error}`);
+  //           }
+  //       });
+
+
+  // exec('var=1 ',
+  //       (error, stdout, stderr) => {
+  //           console.log(stdout);
+  //           console.log(stderr);
+  //           if (error !== null) {
+  //               console.log(`exec error: ${error}`);
+  //           }
+  //       });
+
+  // exec('echo DESPUES DE $valor',
+  //       (error, stdout, stderr) => {
+  //           console.log(stdout);
+  //           console.log(stderr);
+  //           if (error !== null) {
+  //               console.log(`exec error: ${error}`);
+  //           }
+  //       });
+
+
+//  if (result = 0) {
+//   console.log("DA 0 SIGUE COMMIT NORMAL")
+// } else {
+//    console.log("DA 1 -> LANZAR ALERTA DE COMMIT")
+
+
+
+
+ // exec('sh .git/hooks/pre-commit',
+ //        (error, stdout, stderr) => {
+ //            console.log(stdout);
+ //            console.log(stderr);
+ //            if (error !== null) {
+ //                console.log(`exec error: ${error}`);
+ //            }
+ //        });
+
 // }
 
 // process.exit(1);
-// exec("echo $?",(error, stdout, stderr) => {console.log(stdout);console.log(stderr);});
+// return 1
+// return process.exit(1);
+//exec("so [-tag]",(error, stdout, stderr) => {console.log(stdout);console.log(stderr);});
+
 
 
 
 // const version = danger.git.fileMatch("**/*.xcworkspacedata")
-
 // if (version.edited) {
 //   //message("Se han realizado cambios en el proyecto archivo .swift")
 //    warn("Se han realizado cambios en la version del proyecto2")
 //    process.exit(1);
 // } 
-
 
